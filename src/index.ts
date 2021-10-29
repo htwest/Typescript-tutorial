d51d7a18f0783eeb3a3d768188bd7739819ce1b8
Hayden West <haydenw7@gmail.com>
2021-10-28 17:54:49 -0700

    halfway through

--- src/index.ts
+++ src/index.ts
@@ -53,3 +53,58 @@ enum Direction3 {
 // console.log(Direction3.Left)
 
 // OBJECTS
+
+const user:{
+  id: number,
+  name:string
+} = {
+  id: 1,
+  name: 'John',
+}
+
+type User = {
+  id: number,
+  name: string
+}
+const user2: User = {
+  id: 1,
+  name: 'John',
+}
+
+// TYPE ASSERTION
+
+let cid: any = 1
+
+// let customerId = <number>cid
+let customerId = cid as number
+
+// FUNCTIONS
+
+function addNum(x: number, y: number): number {  /* After params is expected return */
+  return x + y
+}
+
+function log(message: string | number): void {  /* Void type means no expected return */
+  console.log(message)
+}
+// log('hello world')
+
+// INTERFACES
+
+interface UserInterface {  /* Interfaces cannot be used with primitives or unions */
+  readonly id: number /*  specifies a read only property */
+  name: string
+  age?: number /*  ? marks an optional property */
+}
+const user1: UserInterface = {
+  id: 1,
+  name: 'John,'
+}
+
+interface MathFunc {
+  (x: number, y: number) : number
+}
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+// CLASSES
